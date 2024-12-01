c1d8da94d0dca3442a4376cc3c5b62f9a1599813
Shelly855 <c3011737@hallam.shu.ac.uk>
2024-12-01 22:26:25 +0000

    modify category contoller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -11,7 +12,9 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $categories = Category::all();
+        $page_title = "Category";
+        return view('category.index', compact('categories', 'page_title'));
     }
 
     /**
