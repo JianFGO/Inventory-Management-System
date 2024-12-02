1e59e1b95312387656a9847084ac50590cfead6e
Shelly855 <c3011737@hallam.shu.ac.uk>
2024-12-02 17:00:45 +0000

    modify store function

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -30,7 +30,9 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string'
+        ]);
     }
 
     /**
