a36d4fe40f12064dc52a0c7dcf8298bf82f26490
Shelly855 <c3011737@hallam.shu.ac.uk>
2024-12-02 17:47:43 +0000

    modify title

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,5 +1,35 @@
 @extends('layouts.master')
 
+@section('title', 'Category')
+
 @section('content')
     <h1>Category</h1>
+    <div class="section-body">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>{{ $page_title }}</h4>
+                    </div>
+                    <div class="card-body">
+                        <table id="myTable" class="table table-striped">
+                            <thead>
+                                <th>Name</th>
+                                <th>Action</th>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+    <script>
+        // For datatable plugin
+        $(document).ready(function() {
+            $('#myTable').DataTable();
+        });
+    </script>
 @endsection
