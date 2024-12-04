fac1f4db948669cce01253beaf426e1cff3d9672
Shelly855 <c3011737@hallam.shu.ac.uk>
2024-12-04 19:25:32 +0000

    add error message

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -12,10 +12,18 @@
                     <form action="{{ route('category.store') }}" method="POST" enctype="multipart/form-data">
                         @csrf
                         <div class="card-body">
+
+                            {{-- Name input --}}
                             <div class="form-group">
                                 <label>Name</label>
                                 <input type="text" name="name" id="name" class="form-control">
                             </div>
+                            {{-- Display an error message if 'name' field is invalid --}}
+                            @error('name')
+                                <p class="text-danger">{{ $message }}</p>
+                            @enderror
+
+                            {{-- Submit button for form --}}
                             <div class="form-group">
                                 <button type="submit" class="btn btn-primary">Submit</button>
                             </div>
