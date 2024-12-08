e6b9b3c3955e9a8ba0cb7091c3be0014c32434ee
Shelly855 <c3011737@hallam.shu.ac.uk>
2024-12-08 17:58:19 +0000

    add edit form

--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -1,1 +1,79 @@
-<h1>Edit</h1>
+@extends('layouts.master');
+
+{{-- Browser tab title --}}
+@section('title', 'Edit Product');
+
+@section('content')
+    <div class="section-body">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+
+                    {{-- Page title --}}
+                    <div class="card-header">
+                        <h4>{{ $page_title }}</h4>
+                    </div>
+
+                    {{-- Form for updating product --}}
+                    <form action="{{ route('product.update', $product->id) }}" method="POST" enctype="multipart/form-data">
+
+                        {{-- CSRF token: Protects form from cross-site request forgery attacks --}}
+                        @csrf
+                        @method('PUT')
+                        <div class="card-body">
+
+                            {{-- Name input --}}
+                            <div class="form-group">
+                                <label>Name</label>
+                                <input type="text" name="name" id="name" class="form-control"
+                                    value="{{ $product->name }}">
+                            </div>
+
+                            {{-- Display an error message if 'name' field is invalid --}}
+                            @error('name')
+                                <p class="text-danger">{{ $message }}</p>
+                            @enderror
+
+                            {{-- Category selection --}}
+                            <div class="form-group">
+                                <label>Category</label>
+                                <select name="category_id" id="category_id">
+                                    @foreach ($categories as $category)
+                                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                    @endforeach
+                                </select>
+                                @error('category_id')
+                                    <p class="text-danger">{{ $message }}</p>
+                                @enderror
+
+                                {{-- Price input --}}
+                                <div class="form-group">
+                                    <label>Price</label>
+                                    <input type="text" name="price" id="price" class="form-control"
+                                        value="{{ $product->price }}">
+                                </div>
+                                @error('price')
+                                    <p class="text-danger">{{ $message }}</p>
+                                @enderror
+
+                                {{-- Quantity input --}}
+                                <div class="form-group">
+                                    <label>Quantity</label>
+                                    <input type="text" name="quantity" id="quantity" class="form-control"
+                                        value="{{ $product->quantity }}">
+                                </div>
+                                @error('quantity')
+                                    <p class="text-danger">{{ $message }}</p>
+                                @enderror
+
+                                {{-- Submit button for form --}}
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary">Update</button>
+                                </div>
+                            </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
