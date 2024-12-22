d7b477f1dfd327e36a89c9925fcec2036f7e25cf
sumayyah19 <c3023364@hallam.shu.ac.uk>
2024-12-22 20:49:40 +0000

    make order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     protected $fillable = [
-        'order_no','branch_id','total_products','paid_amount','delivery_date'
+        'order_no','branch_id','total_amount','paid_amount','delivery_date'
     ];
+    // Defines foreign key relationships
+    public function branch()
+    {
+        return $this->belongsTo('App\Models\Branch');
+    }
+    public function orderDetails()
+    {
+        return $this->hasMany('App\Models\OrderDetails');
+    }
 }
