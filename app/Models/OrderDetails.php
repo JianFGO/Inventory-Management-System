bcdb6827b65f733e056e001a52a9b45a05c4a659
sumayyah19 <c3023364@hallam.shu.ac.uk>
2024-12-22 20:49:58 +0000

    made order details model

--- app/Models/OrderDetails.php
+++ app/Models/OrderDetails.php
@@ -9,4 +9,15 @@ class OrderDetails extends Model
     protected $fillable = [
         'order_id','category_id','product_id','order_quantity','unit_price'
     ];
+
+    // Defines foreign key relationships
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Category');
+    }
+    public function product()
+    {
+        return $this->belongsTo('App\Models\Product');
+    }
 }
+
