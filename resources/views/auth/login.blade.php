7dbdecb56aa96b37650b5f6d5d3e025da89f83e5
Shelly855 <c3011737@hallam.shu.ac.uk>
2024-12-23 18:27:34 +0000

    remove create account

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,30 +1,33 @@
 @extends('layouts.master')
 
 @section('content')
-
-<section class="section">
-      <div class="container mt-5">
-        <div class="row">
-          <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
-            {{-- 
+    <section class="section">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                    {{--
 <div class="login-brand">
     <img src="assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
 </div>
 --}}
 
 
-            <div class="card card-primary">
-              <div class="card-header"><h4>Login</h4></div>
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h4>Login</h4>
+                        </div>
 
-              <div class="card-body">
-              <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                        <div class="card-body">
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
 
 
-                            <label for="email" class="col-form-label text-md-end">{{ __('Email Address') }}</label>
+                                <label for="email" class="col-form-label text-md-end">{{ __('Email Address') }}</label>
 
 
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email"
+                                    class="form-control @error('email') is-invalid @enderror" name="email"
+                                    value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -32,9 +35,12 @@
                                     </span>
                                 @enderror
 
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                                <label for="password"
+                                    class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password"
+                                    class="form-control @error('password') is-invalid @enderror" name="password" required
+                                    autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -44,7 +50,8 @@
 
 
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember"
+                                        {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
                                         {{ __('Remember Me') }}
@@ -53,29 +60,29 @@
 
 
                                 <div class="form-group">
-                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
-                      Login
-                    </button>
-                  </div>
-                    </form>
-                
+                                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                        Login
+                                    </button>
+                                </div>
+                            </form>
 
-              </div>
+
+                        </div>
+                    </div>
+                    <div class="mt-5 text-muted text-center">
+                        No account yet? Reach out to an admin to create one.
+                    </div>
+                    <div class="simple-footer">
+                        Copyright &copy; Stisla 2018
+                    </div>
+                </div>
             </div>
-            <div class="mt-5 text-muted text-center">
-              Don't have an account? <a href="{{route('register')}}">Create One</a>
-            </div>
-            <div class="simple-footer">
-              Copyright &copy; Stisla 2018
-            </div>
-          </div>
         </div>
-      </div>
     </section>
 
 
-            </div>
-        </div>
     </div>
-</div>
+    </div>
+    </div>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
