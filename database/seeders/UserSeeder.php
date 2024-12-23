2b0bb682c2e82c698dc9ce5a2627ee06cee982b1
Shelly855 <c3011737@hallam.shu.ac.uk>
2024-12-23 12:34:58 +0000

    seed data

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -24,22 +24,22 @@ class UserSeeder extends Seeder
                 'role' => 'Admin',
                 'email_verified_at' => now()
             ],
-            // [
-            //     'name' => 'Mannie Carter',
-            //     'email' => 'mannie@gmail.com',
-            //     'password' => Hash::make('Password1'),
-            //     'branch_id' => 2,
-            //     'role' => 'Manager',
-            //     'email_verified_at' => now()
-            // ],
-            // [
-            //     'name' => 'Sal Clarkson',
-            //     'email' => 'sal@gmail.com',
-            //     'password' => Hash::make('Password1'),
-            //     'branch_id' => 3,
-            //     'role' => 'Sales Clerk',
-            //     'email_verified_at' => now()
-            // ],
+            [
+                'name' => 'Mannie Carter',
+                'email' => 'mannie@gmail.com',
+                'password' => Hash::make('Password1'),
+                'branch_id' => 2,
+                'role' => 'Manager',
+                'email_verified_at' => now()
+            ],
+            [
+                'name' => 'Sal Clarkson',
+                'email' => 'sal@gmail.com',
+                'password' => Hash::make('Password1'),
+                'branch_id' => 3,
+                'role' => 'Sales Clerk',
+                'email_verified_at' => now()
+            ],
         ];
 
         // Loop and insert each causer into database
