bcd0ee623783b0a32781926421466c232d4dc591
Shelly855 <c3011737@hallam.shu.ac.uk>
2024-12-24 13:54:40 +0000

    adjust login styles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <section class="section">
         <div class="container mt-5">
             <div class="row">
-                <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-5 offset-lg-2 col-xl-4 offset-xl-3">
+                <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-5 offset-lg-5 col-xl-4 offset-xl-4">
                     <div class="branding text-center mb-4">
                         <h1 style="font-weight: bold; font-size: 24px; color: #333;">Candy Atlas</h1>
                     </div>
