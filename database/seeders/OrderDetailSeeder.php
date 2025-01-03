48b0dbdd9ba33a0d03d0db2d7fcd6525be65d6ff
sumayyah19 <c3023364@hallam.shu.ac.uk>
2025-01-03 20:09:42 +0000

    add order data

---
+++ database/seeders/OrderDetailSeeder.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\OrderDetails;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class OrderDetailSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Default orders to be seeded into database
+        $orderDetails = [
+            // Orders for Sheffield
+            ['order_id' => 1, 'category_id' => 1,'product_id' => 1, 'order_quantity' => '70', 'unit_price' => '0.80'],
+            ['order_id' => 1, 'category_id' => 2,'product_id' => 4, 'order_quantity' => '100', 'unit_price' => '0.50'],
+
+            ['order_id' => 2, 'category_id' => 3,'product_id' => 6, 'order_quantity' => '300', 'unit_price' => '0.85'],
+
+            ['order_id' => 3, 'category_id' => 4,'product_id' => 8, 'order_quantity' => '100', 'unit_price' => '0.75'],
+            ['order_id' => 3, 'category_id' => 5,'product_id' => 10, 'order_quantity' => '120', 'unit_price' => '1.10'],
+            ['order_id' => 3, 'category_id' => 1,'product_id' => 2, 'order_quantity' => '140', 'unit_price' => '1.50'],
+
+
+            // //Orders for Manchester
+            ['order_id' => 4, 'category_id' => 5,'product_id' => 11, 'order_quantity' => '170', 'unit_price' => '1.30'],
+            ['order_id' => 4, 'category_id' => 3,'product_id' => 7, 'order_quantity' => '80', 'unit_price' => '1.05'],
+            ['order_id' => 4, 'category_id' => 2,'product_id' => 5, 'order_quantity' => '400', 'unit_price' => '0.30'],
+
+            ['order_id' => 5, 'category_id' => 1,'product_id' => 3, 'order_quantity' => '175', 'unit_price' => '1.00'],
+            ['order_id' => 5, 'category_id' => 2,'product_id' => 4, 'order_quantity' => '200', 'unit_price' => '0.50'],
+            ['order_id' => 5, 'category_id' => 4,'product_id' => 9, 'order_quantity' => '125', 'unit_price' => '1.60'],
+
+            ['order_id' => 6, 'category_id' => 5,'product_id' => 10, 'order_quantity' => '300', 'unit_price' => '1.10'],
+
+            // //Orders for London
+            ['order_id' => 7, 'category_id' => 2,'product_id' => 5, 'order_quantity' => '400', 'unit_price' => '0.30'],
+            ['order_id' => 7, 'category_id' => 4,'product_id' => 8, 'order_quantity' => '480', 'unit_price' => '0.75'],
+
+            ['order_id' => 8, 'category_id' => 1,'product_id' => 1, 'order_quantity' => '275', 'unit_price' => '0.80'],
+            ['order_id' => 8, 'category_id' => 3,'product_id' => 6, 'order_quantity' => '300', 'unit_price' => '0.85'],
+
+            ['order_id' => 9, 'category_id' => 5,'product_id' => 11, 'order_quantity' => '250', 'unit_price' => '1.30'],
+
+        ];
+
+        // Loop and insert each order into database
+        foreach ($orderDetails as $orderDetail) {
+            OrderDetails::create($orderDetail);
+        }
+    }
+}
---
+++ database/seeders/OrderSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Order;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class OrderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Default orders to be seeded into database
+        $orders = [
+            // Orders for Sheffield            
+            ['branch_id' => 1, 'order_no' => 'ORD_001', 'total_amount' => '106', 'paid_amount' => '50', 'delivery_date' => '2025-01-17'],
+            ['branch_id' => 1, 'order_no' => 'ORD_002', 'total_amount' => '255', 'paid_amount' => '255', 'delivery_date' => '2025-01-22'],
+            ['branch_id' => 1, 'order_no' => 'ORD_003', 'total_amount' => '417', 'paid_amount' => '100', 'delivery_date' => '2025-01-30'],
+
+            //Orders for Manchester
+            ['branch_id' => 2, 'order_no' => 'ORD_004', 'total_amount' => '475', 'paid_amount' => '250', 'delivery_date' => '2025-01-31'],
+            ['branch_id' => 2, 'order_no' => 'ORD_005', 'total_amount' => '475', 'paid_amount' => '420', 'delivery_date' => '2025-02-03'],
+            ['branch_id' => 2, 'order_no' => 'ORD_006', 'total_amount' => '330', 'paid_amount' => '330', 'delivery_date' => '2025-02-20'],
+
+            //Orders for London
+            ['branch_id' => 3, 'order_no' => 'ORD_007', 'total_amount' => '480', 'paid_amount' => '125', 'delivery_date' => '2025-02-27'],
+            ['branch_id' => 3, 'order_no' => 'ORD_008', 'total_amount' => '475', 'paid_amount' => '250', 'delivery_date' => '2025-03-02'],
+            ['branch_id' => 3, 'order_no' => 'ORD_009', 'total_amount' => '325', 'paid_amount' => '325', 'delivery_date' => '2025-03-04'],
+
+        ];
+
+        // Loop and insert each order into database
+        foreach ($orders as $order) {
+            Order::create($order);
+        }
+    }
+}
