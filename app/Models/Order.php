cb0123efcd33420eae7d7d02ea79207c3ad5cd24
sumayyah19 <c3023364@hallam.shu.ac.uk>
2025-01-03 20:11:15 +0000

    update order models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Order extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'order_no',
         'branch_id',
