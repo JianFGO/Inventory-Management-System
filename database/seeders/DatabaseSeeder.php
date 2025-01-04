58c788dcc813f80582a3598842cd0870ae0951af
Shelly855 <c3011737@hallam.shu.ac.uk>
2025-01-04 11:45:55 +0000

    add seeder calls

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,5 +23,7 @@ class DatabaseSeeder extends Seeder
         $this->call([RoleSeeder::class]);
         $this->call([AssignRolesToExistingUsersSeeder::class]);
         $this->call([ProductSeeder::class]);
+        $this->call([OrderSeeder::class]);
+        $this->call([OrderDetailSeeder::class]);
     }
 }
